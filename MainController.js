34f986725e61b408aad0e46bf26054c25ac746ba
Ionut Stoica <ionut.stoica@accesa.eu>
2015-07-09 10:08:16 +0300

    Added MainController.js

--- MainController.js
+++ MainController.js
@@ -5,11 +5,67 @@
 	var MainController = function($scope) {
 
 		$scope.post = {
+
+			author: "Mark Zuckerberg",
+			picture: "http://a4.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTIwNjA4NjMzNjg3ODAzNDA0.jpg",
 			content: "This is some preview text.",
 			likes: 0,
-			comments: ["nice", "cool", "meh"]
+			comments: [
+			{
+
+				name: "Steve",
+				picture: "http://www.campusidea.com/images/male_profile.gif",
+				content: "nice",
+				like: 0
+			},
+			{
+				name: "Kate",
+				picture: "http://www.blackfoot.ca/wp-content/uploads/2014/09/profile-photo.jpg",
+				content: "cool",
+				like: 0
+			},
+			{
+				name: "John",
+				picture: "http://media.cirrusmedia.com.au/LW_Media_Library/LW-603-p28-partner-profile.jpg",
+				content: "meh",
+				like: 0
+			}]
 		};
 
+
+		$scope.postLike = function()
+		{
+			$scope.post.likes += 1;
+			
+		};
+
+		$scope.commLike = function(index)
+		{
+			$scope.comments[1].like += 1;
+			
+		};
+
+		$scope.newComm = '';
+
+		$scope.addComm = function()
+		{
+			
+			var commNr = $scope.post.comments.length;
+			
+			$scope.comments[commNr] =
+			{
+				name: "tet",
+				picture: "da",
+				content: "aaaa",
+				like: 0
+			};
+			alert(commNr);
+
+
+
+		};
+
+		
 
 	};
 
